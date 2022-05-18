90b2a11061ef99c2057f19f4d70cefb21974f941
dev-msj <msj944@naver.com>
2022-05-18 21:26:15 +0900

    feat: main 메서드 추가
    사용자에게 문자열을 입력받아 결과를 출력

---
+++ code/step2/string-calculator/src/main/java/com/next/Main.java
@@ -0,0 +1,18 @@
+package com.next;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String userString = getUserString();
+        System.out.println("result = " + new Calculator(userString).calculate());
+    }
+
+    private static String getUserString() {
+        Scanner scanner = new Scanner(System.in);
+
+        String value = scanner.nextLine();
+        return value;
+    }
+}
