0d1a74a2f8f752861bb80725ae2e1abfd3f3b42f
dev-msj <msj944@naver.com>
2022-05-18 20:20:09 +0900

    feat: calculator 기능 추가
    문자열을 받아 덧셈 결과로 반환하는 기능 추가

--- code/step2/string-calculator/src/main/java/com/next/Calculator.java
+++ code/step2/string-calculator/src/main/java/com/next/Calculator.java
@@ -1,4 +1,32 @@
 package com.next;
 
 public class Calculator {
+    private final String rawString;
+    private int result;
+
+    public Calculator(String rawString) {
+        this.rawString = rawString;
+    }
+
+    public int calculate() {
+        String[] splitResult = getSplitResult();
+        validateSplitResult(splitResult);
+        add(splitResult);
+
+        return this.result;
+    }
+
+    private String[] getSplitResult() {
+        return new Splitter(this.rawString).split();
+    }
+
+    private void validateSplitResult(String[] splitResult) {
+        new Validation(splitResult).check();
+    }
+
+    private void add(String[] splitResult) {
+        for (String value : splitResult) {
+            this.result += Integer.parseInt(value);
+        }
+    }
 }
--- code/step2/string-calculator/src/test/java/com/next/CalculatorTest.java
+++ code/step2/string-calculator/src/test/java/com/next/CalculatorTest.java
@@ -1,19 +1,22 @@
 package com.next;
 
 import com.next.Calculator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatorTest {
-    private static Calculator calculator;
-
-    @BeforeAll
-    public static void setup() {
-        calculator = new Calculator();
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3", "1:2:3", "1,2:3"})
+    public void addTest(String input) {
+        Assertions.assertThat(new Calculator(input).calculate()).isEqualTo(6);
     }
 
-    @Test
-    public void test() {
-
+    @ParameterizedTest
+    @ValueSource(strings = {"//a\n1a2a4", "//\n124", "//;;\n1;;2;;4"})
+    public void addCustomSplitTest(String input) {
+        Assertions.assertThat(new Calculator(input).calculate()).isEqualTo(7);
     }
 }
