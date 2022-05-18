622519da6dba0c0be0f9680765f7f5dbeed5ba44
dev-msj <msj944@naver.com>
2022-05-19 02:38:53 +0900

    refactor: 입력받은 문자열이 empty면 0을 출력하도록 변경

--- code/step2/string-calculator/src/main/java/com/next/Main.java
+++ code/step2/string-calculator/src/main/java/com/next/Main.java
@@ -6,12 +6,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String userString = getUserString();
-        System.out.println("result = " + new Calculator(userString).calculate());
+        int result = getResult(userString);
+        System.out.println("result = " + result);
     }
-
+    
     private static String getUserString() {
         Scanner scanner = new Scanner(System.in);
 
         return scanner.nextLine().replace("\\n", "\n");
+    }
+
+    private static int getResult(String userString) {
+        return userString.isEmpty() ? 0 : new Calculator(userString).calculate();
     }
 }
