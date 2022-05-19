47fe57135971f6cdd6858a18c5f0e7a463b80b4d
dev-msj <msj944@naver.com>
2022-05-20 01:37:32 +0900

    구분자를 쉼표 이외에 콜론을 사용

--- code/step2/string-calculator/src/test/java/com/book/StringCalculatorTest.java
+++ code/step2/string-calculator/src/test/java/com/book/StringCalculatorTest.java
@@ -27,4 +27,9 @@ public class StringCalculatorTest {
     public void add_쉼표구분자() throws Exception {
         Assertions.assertEquals(3, cal.add("1,2"));
     }
+
+    @Test
+    public void add_쉼표구분자_또는_콜론_구분자() throws Exception {
+        Assertions.assertEquals(6, cal.add("1,2:3"));
+    }
 }
